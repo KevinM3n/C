ec87701c7f121b4b5998fd6886c9d8050f4c17f0
U-BSD\465783 <465783@SHS-10L74C39E.beaverton.k12.or.us>
2018-10-04 12:25:01 -0700

    My first completed commit cuz I'm a dumbass and I never used git before

---
+++ studentlist/studentlistCheng.cpp
@@ -0,0 +1,278 @@
+/*Saw KEVIN MEN'S CODE FOR HELP ON VECTORS
+
+
+ */
+
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<string.h>
+using namespace std;
+
+struct student
+{
+  char firstname [99];
+  char lastname [99];
+  int id;
+  float gpa;
+};
+
+student* add(vector<student*>studentlist, int number); // when you want to add a student.
+    
+void print(vector<student*> studentlist); // when you want to print out the student list
+
+
+
+  int main()
+  {
+    cout << "This program creates a student list and allows uesr to add/remove students into/from the student list." << endl;
+    cout << "Type \"ADD\" if you want to add a student to the student list." << endl;
+    cout << "Type \"DELETE\" if you want to delete one of the student in the student list."<< endl;
+    cout << "Type \"PRINT\" if you want to see all of the students' information in the student list."<< endl;
+    cout << "Type \"QUIT\" or simply alt+f4 to close the program."<< endl;
+    cout << endl;
+
+    vector<student*> studentlist;
+    bool program = true;
+    char answer[99];
+    while (program == true)
+      {
+	
+	
+	int numberofstudent = 0;
+		for (vector<student*>::iterator it = studentlist.begin(); it != studentlist.end(); ++it)
+	      {
+		numberofstudent ++;
+	      }
+	  
+	      cout << "You currently have " <<numberofstudent << " students in your studentlist" << endl;
+
+	cout << "Now what would you like to do? My friend."<< endl;
+	
+	
+  	cin.get (answer, 99);
+	cin.get();
+	int length = strlen(answer);
+	answer[length] = '\0';// end the char array from reading in more.
+	//Ex. answer = ADD, answer length = 3, answer[3] would be empty and just end right there.
+	//cout << answer << length << endl; // just testing
+	 // if the user's response starts with ADD, go add a student, same thing for deleteing and printing.
+
+	  
+
+	if (answer[0] == 'A' &&
+	    answer[1] == 'D' &&
+	    answer[2] == 'D' )
+	  {
+	 
+	    studentlist.push_back(add(studentlist, numberofstudent)); // add
+	    
+	    
+	  }
+	else if (answer[0] == 'D' &&
+	    answer[1] == 'E' &&
+	    answer[2] == 'L' &&
+	    answer[3] == 'E' &&
+	    answer[4] == 'T' &&
+	    answer[5] == 'E' )
+	  {
+	    if (numberofstudent != 0) // if there is a student in studentlist
+	      {
+	 	cout << "What is the ID of the student that you want to delete?" << endl;
+		int id = 0;
+		cin >>(id); // read in the id
+		cin.get();
+		int track = 0; // Keep track of which student it is
+		for (vector<student*>::iterator it = studentlist.begin(); it != studentlist.end(); ++it)
+	      {
+	       
+		if ((*it) -> id == id)
+		  {
+		    
+		    studentlist.erase(studentlist.begin() + track); // start from 0 + the index
+		    break; // so the code doesn't Kill it self and tell me core dumped
+		    }
+		else if (it == studentlist.end()-1)
+		  {
+		    cout << "Couldn't find the student that you want to delete, maybe try printing out the studentlist first!" << endl;
+		  }
+		track ++;
+	      }
+	   
+		
+	      }
+	    else
+	      {
+		cout << "You don't have any student in your studnetlist! Please add some." << endl;
+	      }
+	  }
+	 else if (answer[0] == 'P' &&
+	    answer[1] == 'R' &&
+	    answer[2] == 'I' &&
+	    answer[3] == 'N' &&
+	    answer[4] == 'T' )
+	  {
+	    /* Go through the studentlist (vector) from the beginning to the end
+	       Print out their information.
+
+	     */
+	    if (numberofstudent != 0)
+	      {
+	    for (vector<student*>::iterator it = studentlist.begin(); it != studentlist.end(); ++it)
+	      {
+		cout <<"Name: "<< (*it)-> firstname <<" " <<(*it)-> lastname << "\tID: " <<(*it) -> id <<  "\tGPA: " << (*it)-> gpa <<endl;
+
+	      }
+	      }
+	    else  // if has student print
+	      {
+		cout << "You don't have any student in your studentlist! Please add some!" << endl;
+	      }
+
+	  }
+	else if (answer[0] == 'Q' &&
+	    answer[1] == 'U' &&
+	    answer[2] == 'I' &&
+	    answer[3] == 'T' )
+	  {
+	    cout << "Glad to help you, have a good day!" << endl;
+	    return 0;
+	  }
+	else  // if it's none of them, tell user to retype.
+	  {
+	    cout << "I don't really understand, maybe try capitalize everything" << endl;
+
+	  }
+      
+       }
+  }
+    
+
+   
+student* add(vector<student*> studentlist, int number) // when you want to add a student
+{
+  /*
+    SAW KEVIN MEN'S GITHUB  EXAMPLES OF FOR LOOPS FOR VECTORS & USE OF ->, * & PUSHBACK 
+
+
+
+
+   */
+  /*
+    when you add a student, first set the return type to student so back there you do 
+    studentlist.push_back(add(studentlist)) add(studentlist) gives you a student
+    
+    I need to pass in the studetnlist so i can check if theres a student in studentlist
+    with the same id number which is not allowed/
+
+
+  */
+  char firstname[99];
+  char lastname[99];
+  int id;
+  float gpa;
+  /*
+    Variables of a student
+   */
+  student* a = new student;
+  // create a new student
+  
+    cout << "What's the first name of the students that you want to add?" << endl;
+ cin.get(firstname,99);
+ cin.get();
+
+ int flength = strlen(firstname);
+firstname[flength] = '\0';
+
+    cout << "What's the last name of the students that you want to add?" << endl;
+ cin.get(lastname,99);
+ cin.get();
+
+ int llength = strlen(lastname);
+ lastname[llength] = '\0';
+ /*
+   ask for information, and stop reading in more first name / last name after it's done('\0')
+  */
+ for(int i=0; i<= flength; i++)
+   {
+     a->firstname[i] = firstname[i];
+   }
+ for(int i=0; i<= llength; i++)
+   {
+     a->lastname[i] = lastname[i];
+   }
+ /*
+   make the student's first name last name the same as what we just read in
+  */
+     
+
+
+ cout << "ID?" << endl;
+
+ 
+ bool rep = true;
+ while (rep == true)
+   {
+     cin >> id;
+     cin.get();
+
+	 /*
+	   keep doing it until the user types in something that's different than the id existed.
+	 
+	 */
+     if (number == 0) // read in at the beginning so I know if theres any students in the studentlist
+       // if there isn't any student there is no point checking for repeating id
+	   {
+	     
+	     break;
+	   }
+    for (vector<student*>::iterator it = studentlist.begin(); it != studentlist.end(); ++it)
+   {
+       if  ((*it)-> id == id)
+       {
+	 // if we find one number that's the same as what user type in there is no point keep going
+	 // just get out and tell him to retype
+	 rep = true;
+	 break;
+       }
+       else if (it == studentlist.end() -1  ) 
+	 {
+	   // if we can't find one and it's the end of the studentlist, he is clean, he can get out
+	   rep =false;
+	 }
+    }
+ /* read in the id, go through the studentlist, if the id already exists
+	    tell the user to retype
+
+	   
+	  */
+    if (rep == true) // if he needs to retype tell him this
+	   {
+	     cout << "Some one with the same ID number is already in the studentlist!" << endl;
+	 cout << "Please type in another student ID" << endl;
+	 
+	   }
+
+	 
+   }
+   a->id = id;
+
+   /*
+     if the id is unique, set the student's id to the id we read in
+    */
+	   
+ 
+   // read in gpa 
+ cout << "GPA?" << endl;
+ cin >> gpa;
+ cin.get();
+   a->gpa =gpa;
+
+  
+   // return the student t
+        }
+
+
+       
+
+
--- tictactoe/tictactoeCheng.cpp
+++ tictactoe/tictactoeCheng.cpp
@@ -1,325 +1,569 @@
-
-
 #include<cstring>
 #include<iostream>
 #include<string.h>
 
-using namespace std;
+ using namespace std;
 
+ 
 const int X_MOVE = 1;
+
 const int O_MOVE = 2;
+
 const int BLANK = 0;
-const int X_TURN = 0 ;
+
+const int X_TURN = 0;
+
 const int O_TURN = 1;
 
-bool checkwin(int board[][3], int player )
+ 
+bool checkwin (int board[][3], int player) 
 /*  check if the user wins by one of the possibilities out of 9
- */
+ */ 
 {
-		bool win = false;
-		if (board[0][0] == player &&
-			board[1][0] == player &&
-			board[2][0] == player )
-			{
-			win = true;
-			}
-		if (board[0][1]== player &&
-			board[1][1] == player &&
-			board[2][1] == player )
-			{
-			win = true;
-			}
-		if (board[0][2]== player &&
-				board[1][2] == player &&
-				board[2][2] == player )
-				{
-				win = true;
-				}
-		if (board[0][0]== player &&
-				board[0][1] == player &&
-				board[0][2] == player )
-				{
-				win = true;
-				}
-		if (board[1][0]== player &&
-				board[1][1] == player &&
-				board[1][2] == player )
-				{
-			win = true;
-				}
-		if (board[2][0]== player &&
-				board[2][1] == player &&
-				board[2][2] == player )
-				{
-			win = true;
-				}
-		if (board[0][0]== player &&
-				board[1][1] == player &&
-				board[2][2] == player )
-				{
-				win = true;
-				}
-		if (board[2][0]== player &&
-				board[1][1] == player &&
-				board[0][2] == player )
-				{
-			win = true;
-				}
-		
-		return win;
+  
+bool win = false;
+  
+if (board[0][0] == player && 
+board[1][0] == player
+       && 
+board[2][0] == player)
+    
+    {
+      
+win = true;
+    
+}
+  
+if (board[0][1] == player && 
+board[1][1] == player
+       && 
+board[2][1] == player)
+    
+    {
+      
+win = true;
+    
+}
+  
+if (board[0][2] == player && 
+board[1][2] == player
+       && 
+board[2][2] == player)
+    
+    {
+      
+win = true;
+    
+}
+  
+if (board[0][0] == player && 
+board[0][1] == player
+       && 
+board[0][2] == player)
+    
+    {
+      
+win = true;
+    
+}
+  
+if (board[1][0] == player && 
+board[1][1] == player
+       && 
+board[1][2] == player)
+    
+    {
+      
+win = true;
+    
+}
+  
+if (board[2][0] == player && 
+board[2][1] == player
+       && 
+board[2][2] == player)
+    
+    {
+      
+win = true;
+    
+}
+  
+if (board[0][0] == player && 
+board[1][1] == player
+       && 
+board[2][2] == player)
+    
+    {
+      
+win = true;
+    
+}
+  
+if (board[2][0] == player && 
+board[1][1] == player
+       && 
+board[0][2] == player)
+    
+    {
+      
+win = true;
+    
+}
+  
+ 
+return win;
+
+ 
+}
+
+
+ 
+bool checktie (int board[][3]) 
+{
+  
+    /*
+       if all the boards is filled, and no one has win, it's a tie
+     */ 
+    bool tie = true;
+  
+for (int row = 0; row < 3; row++)
+    
+    {
+      
+for (int column = 0; column < 3; column++)
+	
+	{
+	  
+if (board[row][column] == BLANK)
+	    
+	    {
+	      
+tie = false;
+	    
+}
 	
 }
-
-bool checktie(int board[][3])
-{
-  /*
-    if all the boards is filled, and no one has win, it's a tie
-   */
-    bool tie = true;
-    for ( int row = 0; row < 3 ; row++)
-    {
-         for (int column = 0; column < 3; column ++)
-         {
-             if (board[row][column] == BLANK)
-             {
-                 tie = false;
-             }
-         }
-    }
-    return tie;
     
 }
-int clearboard ( int board[][3])
-{// set everything on the board to blank;
-       for (int row = 0; row < 3; row ++)
-        {
-            for (int column = 0; column < 3; column ++)
-        	{
-	          board[row][column] = BLANK;
-	        }
-             
-        }
+  
+return tie;
+
+ 
 }
-int printboard (int board[][3])
-{
-  // print out the board
-  // print out the top of th eboard
-    cout << "\t1\t2\t3" << endl;
-        for ( int row = 0; row < 3 ; row++)
-        {
-	  //print out the side of the board. for each row, print a + row number.
-          cout << (char)('a' + row) <<"\t";
-	  // if it's blank print nothing, if it's X print X if it's O print O
-           for (int column = 0; column < 3; column ++)
-        	{
-    	    if (board[row][column] == BLANK)
-    	        {
-		  cout << "\t";
-    	        }
-    	  else if (board[row][column] == X_MOVE)
-    	        {
-		  cout << "X\t" ;
-	         }
-    	  else if (board[row][column] == O_MOVE)
-    	         {
-		   cout << "O\t";
-		 }
-            
-    	    }
-	   cout << endl;
-       
+
+
+int
+clearboard (int board[][3]) 
+{				// set everything on the board to blank;
+  for (int row = 0; row < 3; row++)
+    
+    {
       
-        }   
-}
-int main()
+for (int column = 0; column < 3; column++)
+	
+	{
+	  
+board[row][column] = BLANK;
 
+} 
+ 
+} 
+} 
+int
+
+printboard (int board[][3]) 
 {
-  int X_SCORE = 0;
-  int O_SCORE = 0;
-  int TURN = X_TURN;
-  int board[3][3];
- 
-
-  bool game = true;
-  clearboard(board);
-  // reset everything
-  cout << "Start the game by entering any of coordinates that's blank in the 3v3 board." << endl;
-      cout << "With a lower case letter followed up by a number. " << endl;
   
-  while (game == true)
+    // print out the board
+    // print out the top of th eboard
+    cout << "\t1\t2\t3" << endl;
+  
+for (int row = 0; row < 3; row++)
+    
     {
-     
-     printboard(board);
-     //tell user whose turn it is
-      if (TURN == X_TURN)
-      {
-  cout << "It's X's turn now " << endl;
-      }
-      else
-      {
-           cout << "It's O's turn now " << endl;
-      }
-      char where [2] ;  //coordinates
-      char user [99]; // whatever user may enter
-      int length = 0; // length
-      cin.get(user,99);  //read in whatever user may enter
- cin.get();
- for (length = 0; length < strlen(user); length++)
-    {
-    }
+      
+	//print out the side of the board. for each row, print a + row number.
+	cout << (char) ('a' + row) << "\t";
+      
+	// if it's blank print nothing, if it's X print X if it's O print O
+	for (int column = 0; column < 3; column++)
+	
+	{
+	  
+if (board[row][column] == BLANK)
+	    
+	    {
+	      
+cout << "\t";
+	    
+}
+	  
+	  else if (board[row][column] == X_MOVE)
+	    
+	    {
+	      
+cout << "X\t";
+	    
+}
+	  
+	  else if (board[row][column] == O_MOVE)
+	    
+	    {
+	      
+cout << "O\t";
+	    
+}
+	
  
- user[length] ='\0';  // stop reading in more
- where[0] = user[0]; // only care about the first two letter/number the user put in
-  where[1] = user [1];
-    int Row = 0;
-    int Column = 0;
-  
-    // if user can't enter a correct coordinate, keep doing it. The user [99] stops the while loop from never ending
-        while (where[0]!= 'a' &&
-        	where[0]!= 'b' &&
-        	where[0]!= 'c' )
-        		{
-        				cout << "Row must be an a or b or c." << endl;
-        				   length = 0;
- cin.get(user,99);
- cin.get();
- for (length = 0; length < strlen(user); length++)
-    {
-    }
+}
+      
+cout << endl;
+    
  
-  user[length] ='\0';
-  
-   where[0] = user[0];
-  where[1] = user [1];
-        				
-        		}
-        while (where[1]!= '1' &&
-        		where[1]!= '2' &&
-        		where[1]!= '3' )
-        		{
-        	           cout << "Column must be a 1 or 2 or 3." << endl;
-        		   
-			    where[3] = '\0';
-			    length = 0;
-			   
-					cin.get(user,99);
- cin.get();
- for (length = 0; length < strlen(user); length++)
-    {
-    }
  
-  user[length] ='\0';
-  where[0] = user[0];
-  where[1] = user [1];        		}
-        
-        		    Row = where[0] - 'a';
-        		    Column = where[1] - '1';
-                   // cout << where[0]- 'a' << "," << where[1] - '1' << endl;
-                    
-                    if (board[Row][Column] == BLANK)
-                    {
-                     //   cout << where[0]- 'a' << "," << where[1] - '1' << endl;
-		      // if the place
-                        if (TURN == X_TURN)
-                        {
-                        
-                        board[Row][Column] = X_MOVE;
-                        TURN = O_TURN;
-                           if (checkwin(board, X_MOVE) == 1)
-                            {
-                               printboard(board);
-                               X_SCORE += 1;
-                               cout << "X just won, the score is now: X: " <<X_SCORE<<" O: " << O_SCORE << endl;
-                                
-                                clearboard(board);
-                                cout << "Wanna keep playing? Type \"Y\" or \"y\" to continue! " << endl; 
-                                   char go [1];
-                                   cin.get(go,2);
-                                   cin.get();
-                                   if (go[0] != 'y' && go[0] != 'Y'  )
-                                   {
-                                       return 0;
-                                   }
-                            }
-                             else if (checktie(board) == 1)
-                            {
-                                 printboard(board);
-                                   
-                                    cout << "It was a tie! the score is now: X: " <<X_SCORE<<" O: " << O_SCORE << endl;
-                                   
-                                    clearboard(board);
-                                    cout << "Wanna keep playing? Type \"Y\" or \"y\" to continue! " << endl; 
-                             char go [1];
-                                cin.get(go,2);
-                                cin.get();
-                                 if (go[0] != 'y' && go[0] != 'Y'  )
-                                     {
-                                    return 0;
-                                      }
-                                
-                            }
-                            
-                        }
-                        else if (TURN == O_TURN)
-                        {
-                           board[Row][Column] = O_MOVE;
-                            TURN = X_TURN;
-                           if (checkwin(board,O_MOVE) == 1)
-                           {
-                                    printboard(board);
-                                    O_SCORE += 1;
-                                    cout << "Y just won, the score is now: X: " <<X_SCORE<<" O: " << O_SCORE << endl;
-                                   
-                                    clearboard(board);
-                                    cout << "Wanna keep playing? Type \"Y\" or \"y\" to continue! " << endl; 
-                             char go [1];
-                                cin.get(go,2);
-                                cin.get();
-                                 if (go[0] != 'y' && go[0] != 'Y'  )
-                                     {
-                                    return 0;
-                                      }
-                            }
-                            else if (checktie(board) == 1)
-                            {
-                                 printboard(board);
-                                   
-                                    cout << "It was a tie! the score is now: X: " <<X_SCORE<<" O: " << O_SCORE << endl;
-                                   
-                                    clearboard(board);
-                                    cout << "Wanna keep playing? Type \"Y\" or \"y\" to continue! " << endl; 
-                             char go [1];
-                                cin.get(go,2);
-                                cin.get();
-                                 if (go[0] != 'y' && go[0] != 'Y'  )
-                                     {
-                                    return 0;
-                                      }
-                                
-                            }
-                            
-                            
-                        }
-                    }
-                    else
-                    {
-                        cout << "Someone is there already!" << endl;
-                    }
-                 
-    
-    }
-        
-  
-  
-  
-  
-  
- 
-    
-    
-    
-    
+}
 
 }
 
 
+int
+main () 
+ 
+{
+  
+int X_SCORE = 0;
+  
+int O_SCORE = 0;
+  
+int TURN = X_TURN;
+  
+int board[3][3];
+  
+ 
+ 
+bool game = true;
+  
+clearboard (board);
+  
+    // reset everything
+    cout <<
+    "Start the game by entering any of coordinates that's blank in the 3v3 board."
+    << endl;
+  
+cout << "With a LOWER CASE letter followed up by a number. " << endl;
+  
+ 
+while (game == true)
+    
+    {
+      
+ 
+printboard (board);
+      
+	//tell user whose turn it is
+	if (TURN == X_TURN)
+	
+	{
+	  
+cout << "It's X's turn now " << endl;
+	
+}
+      
+      else
+	
+	{
+	  
+cout << "It's O's turn now " << endl;
+	
+}
+      
+char where[2];		//coordinates
+      char user[99];		// whatever user may enter
+      int length = 0;		// length
+      cin.get (user, 99);	//read in whatever user may enter
+      cin.get ();
+      
+for (length = 0; length < strlen (user); length++)
+	
+	{
+	
+}
+      
+ 
+user[length] = '\0';	// stop reading in more
+      where[0] = user[0];	// only care about the first two letter/number the user put in
+      where[1] = user[1];
+      
+int Row = 0;
+      
+int Column = 0;
+      
+ 
+	// if user can't enter a correct coordinate, keep asking user for it. The user [99] stops the while loop from never ending, unless the user is boring
+	// enough to type in 99 characters
+	while (where[0] != 'a' && 
+where[0] != 'b' && 
+where[0] != 'c')
+	
+	{
+	  
+cout << "Row must be an a or b or c." << endl;
+	  
+length = 0;
+	  
+cin.get (user, 99);
+	  
+cin.get ();
+	  
+for (length = 0; length < strlen (user); length++)
+	    
+	    {
+	    
+}
+	  
+ 
+user[length] = '\0';
+	  
+ 
+where[0] = user[0];
+	  
+where[1] = user[1];
+	
+ 
+}
+      
+while (where[1] != '1' && 
+where[1] != '2' && 
+where[1] != '3')
+	
+	{
+	  
+cout << "Column must be a 1 or 2 or 3." << endl;
+	  
+ 
+where[3] = '\0';
+	  
+length = 0;
+	  
+ 
+cin.get (user, 99);
+	  
+cin.get ();
+	  
+for (length = 0; length < strlen (user); length++)
+	    
+	    {
+	    
+}
+	  
+ 
+user[length] = '\0';
+	  
+where[0] = user[0];
+	  
+where[1] = user[1];
+	}
+      
+ //since it's an int array[][], we subtract an a from the row number and 1 from the column number
+ //if it's a1 it would be 0,0
+Row = where[0] - 'a';
+      
+Column = where[1] - '1';
+      
+	// cout << where[0]- 'a' << "," << where[1] - '1' << endl;
+	
+if (board[Row][Column] == BLANK)  // if the coordinate is a blank, the user can then place himself there.
+	
+	{
+	  
+	    //   cout << where[0]- 'a' << "," << where[1] - '1' << endl;
+	    // if the place
+	    if (TURN == X_TURN)
+	    
+	    {
+	      
+  // if it's X's turn, place a X at the coordinates and check to see if X wins by placing there.
+board[Row][Column] = X_MOVE;
+	      // switch turn
+TURN = O_TURN;
+	      
+if (checkwin (board, X_MOVE) == 1)
+		
+		{
+		  // if X wins, print score and ask if they want to play again.
+printboard (board);
+		  
+X_SCORE += 1;
+		  
+cout << "X just won, the score is now: X: " << X_SCORE <<
+		    " O: " << O_SCORE << endl;
+		  
+ 
+clearboard (board);
+		  
+cout <<
+		    "Wanna keep playing? Type \"Y\" or \"y\" to continue! " <<
+		    endl;
+		  
+char go[1];
+		  
+cin.get (go, 2);
+		  
+cin.get ();
+		  
+if (go[0] != 'y' && go[0] != 'Y')
+		    
+		    {
+		      
+return 0;
+		    
+}
+		
+}
+	      // if X did not win and all coordinates are taken, it's a tie,
+	      //tell users that it's a tie and ask if they want to play again.
+	      else if (checktie (board) == 1)
+		
+		{
+		  
+printboard (board);
+		  
+ 
+cout << "It was a tie! the score is now: X: " << X_SCORE
+		    << " O: " << O_SCORE << endl;
+		  
+ 
+clearboard (board);
+		  
+cout <<
+		    "Wanna keep playing? Type \"Y\" or \"y\" to continue! " <<
+		    endl;
+		  
+char go[1];
+		  
+cin.get (go, 2);
+		  
+cin.get ();
+		  
+if (go[0] != 'y' && go[0] != 'Y')
+		    
+		    {
+		      
+return 0;
+		    
+}
+		
+ 
+}
+	    
+ 
+}
+	  // basically same thing as X 
+	  else if (TURN == O_TURN)
+	    
+	    {
+	      
+board[Row][Column] = O_MOVE;
+	      
+TURN = X_TURN;
+	      
+if (checkwin (board, O_MOVE) == 1)
+		
+		{
+		  
+printboard (board);
+		  
+O_SCORE += 1;
+		  
+cout << "Y just won, the score is now: X: " << X_SCORE <<
+		    " O: " << O_SCORE << endl;
+		  
+ 
+clearboard (board);
+		  
+cout <<
+		    "Wanna keep playing? Type \"Y\" or \"y\" to continue! " <<
+		    endl;
+		  
+char go[1];
+		  
+cin.get (go, 2);
+		  
+cin.get ();
+		  
+if (go[0] != 'y' && go[0] != 'Y')
+		    
+		    {
+		      
+return 0;
+		    
+}
+		
+}
+	      
+	      else if (checktie (board) == 1)
+		
+		{
+		  
+printboard (board);
+		  
+ 
+cout << "It was a tie! the score is now: X: " << X_SCORE
+		    << " O: " << O_SCORE << endl;
+		  
+ 
+clearboard (board);
+		  
+cout <<
+		    "Wanna keep playing? Type \"Y\" or \"y\" to continue! " <<
+		    endl;
+		  
+char go[1];
+		  
+cin.get (go, 2);
+		  
+cin.get ();
+		  
+if (go[0] != 'y' && go[0] != 'Y')
+		    
+		    {
+		      
+return 0;
+		    
+}
+		
+ 
+}
+	    
+ 
+ 
+}
+	
+}
+      
+      else  // if the user's coodinate is taken already tell him that it's taken
+	
+	{
+	  
+cout << "Someone is there already!" << endl;
+	
+}
+    
+ 
+ 
+}
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
